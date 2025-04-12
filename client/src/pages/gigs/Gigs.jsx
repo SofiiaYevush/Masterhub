68f7df2127aae9e9177865300b6f23a1d63a16ff
Sofiia13 <sofiia.yevush@gmail.com>
2025-04-12 19:41:13 +0300

    added services page

---
+++ client/src/pages/gigs/Gigs.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Gigs.scss";
+import GigCard from "../../components/gigCard/GigCard";
+import SortDropdown from "../../components/sort-dropdown/SortDropdown";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import { useLocation } from "react-router-dom";
+import { categories as getCategories } from "../../data";
+
+function Gigs() {
+  const [sort, setSort] = useState("sales");
+  const [setOpen] = useState(false);
+  const [setFiltersVisible] = useState(false);
+  const minRef = useRef();
+  const maxRef = useRef();
+  const { search } = useLocation();
+  const categoryKey = new URLSearchParams(search).get("cat");
+
+  const cats = getCategories();
+  const category = cats.find((cat) => cat.key === categoryKey);
+
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs", categoryKey, sort.field, sort.order],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort.field}&order=${sort.order}&category=${categoryKey}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
+  const reSort = (field, order = "desc") => {
+    setSort({ field, order });
+    setOpen(false);
+    setFiltersVisible(false);
+    };
+    
+    useEffect(() => {
+      refetch();
+    }, [sort.field, sort.order, categoryKey]);
+    
+    const apply = () => {
+      refetch();
+    };
+    
+    const resetFilters = () => {
+      if (minRef.current) minRef.current.value = "";
+      if (maxRef.current) maxRef.current.value = "";
+      setSort("sales");
+      refetch();
+    };  
+    
+    return (
+      <div className="services">
+        <div className="services-container">
+          <h1 className="services-title">{category ? category.title : "All Services"}</h1>
+          <p className="services-description">{category ? category.desc : "Browse various gigs and services."}</p> 
+          <div className="menu">
+            <div className="left">
+              <span className="filter-price-title">Price: </span>
+              <div className="filter-price-inputs">
+                <input className="filter-price-input" ref={minRef} type="number" placeholder="min" />
+                <input className="filter-price-input" ref={maxRef} type="number" placeholder="max" />
+              </div>
+              <button className="filter-price-button" onClick={apply}>
+                <img src="../../icons/filter-search.png" alt="" className="filter-price-icon" />
+              </button>
+            </div>
+            <div className="right">
+            <SortDropdown sort={sort} onChange={reSort} />
+            <button onClick={resetFilters} className="reset-button">
+              <img className="reset-icon" src="../../icons/clear-filter.png" alt="" />
+            </button>              
+            </div>
+          </div>
+          <div className="cards">
+            {isLoading
+              ? "loading"
+              : error
+              ? "Something went wrong!"
+              : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default Gigs;
