8a48a616127f7006e4c8671ed1772d3d257c972b
Sofiia13 <sofiia.yevush@gmail.com>
2025-04-28 14:46:22 +0300

    added alert component

---
+++ client/src/components/alert-message/AlertMessage.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import "./AlertMessage.scss";
+
+const AlertMessage = ({ message, duration = 3000, onClose }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      if (onClose) {
+        onClose();
+      }
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration, onClose]);
+
+  if (!visible) return null;
+
+  return (
+    <div className="alert-container">
+      {message}
+    </div>
+  );
+};
+
+export default AlertMessage;
