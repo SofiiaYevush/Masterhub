ec6bca558c4d85e82483cc28ce281a8b0dd74d1d
Sofiia13 <sofiia.yevush@gmail.com>
2026-09-13 00:25:04 +0300

    Updated variable

--- api/server.js
+++ api/server.js
@@ -1,22 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+
 import userRoute from "./routes/user.route.js";
 import gigRoute from "./routes/gig.route.js";
 import orderRoute from "./routes/order.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import adminRoute from "./routes/admin.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
-dotenv.config();
+
 mongoose.set("strictQuery", true);
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongoDB!");
   } catch (error) {
     console.log(error);
@@ -24,8 +29,8 @@ const connect = async () => {
 };
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -34,6 +39,8 @@ app.use("/api/orders", orderRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/profile", userRoute);
 app.use("/api/reviews", reviewRoute);
+app.use("/api/jobs", jobRoute);
+app.use("/api/applications", applicationRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
